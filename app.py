32f12429c30ad13e9349833e28217292d0a62662
Domin01 <alejandroomin@gmail.com>
2021-04-28 17:17:57 +0200

    Error a proposito

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def categoria(tipo):
     return render_template("categorias.html",categoria=tipo,lista_categorias=listacategorias,lista_libros=datos)
 
 port=os.environ["PORT"]
-app.run('0.0.0.0',int(port), debug=True)
+@app.run('0.0.0.0',int(port), debug=True)
