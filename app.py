52c164460f9c79808adf3eb02d6246c15c17cc47
Domin01 <alejandroomin@gmail.com>
2021-04-28 16:25:40 +0200

    Cambiando las opciones de los puertos

--- app.py
+++ app.py
@@ -27,15 +27,4 @@ def categoria(tipo):
     return render_template("categorias.html",categoria=tipo,lista_categorias=listacategorias,lista_libros=datos)
 
 
-
-
-app.run("0.0.0.0",8000,debug=True)
-
-
-#Posible mejora (no es obligatorio, pero estaría muy bien que lo hagáis)
-
-#Cuando se muestra el detalle de un libro y se muestran las categorías, estás son un enlace a la ruta /categoria/<categoria>. 
-#Por ejemplo, si el primer libro tiene una categoría  Mobile, al pulsar sobre el enlace nos lleva a la página /categoria/Mobile
-
-#Libros por categoría: Esta página está en la ruta /categoria/<categoria>: Es muy similar a la página principal, 
-#pero sólo muestra la los libros de esa ccategoría. Además debe a aprecer un título con la categoría.
+app.run('0.0.0.0',int(port), debug=True)
