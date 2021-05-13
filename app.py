8778c4fc7615ce7128c00f39f777bc6a1eaed737
Domin01 <alejandroomin@gmail.com>
2021-05-13 22:49:41 +0200

    Modificamos el fichero app.y para provocar un error de codigo 7

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template,abort,json
-import os
-	
 
-f = open('books.json',)
 
 datos = json.load(f)
 
-@app.route('/',methods=["GET","POST"])
+app.route('/',methods=["GET","POST"])
 def inicio():
 	return render_template("inicio.html",lista_libros=datos)
 
@@ -26,5 +22,5 @@ def categoria(tipo):
                 listacategorias.append(cate)
     return render_template("categorias.html",categoria=tipo,lista_categorias=listacategorias,lista_libros=datos)
 
-#port=os.environ["PORT"]
-app.run('0.0.0.0', debug=True)
+port=os.environ["PORT"]
+app.run('0.0.0.0',int(port), debug=True)
